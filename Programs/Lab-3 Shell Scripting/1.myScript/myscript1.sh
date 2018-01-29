a07e60cd721723a1966f6aacd0d00424ca50f423
jibesh-bhattacharjee <jibesh.nitm@gmail.com>
2018-01-29 09:53:06 +0530

    Adding files

---
+++ Programs/Lab-3 Shell Scripting/1.myScript/myscript1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello Jibesh. This is the listing of your current directory"
+ls 
+echo "Listing of root directory is"
+cd /
+ls 
+
+
---
+++ Programs/Lab-3 Shell Scripting/3.Redirection/script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sort fruits.txt | uniq > uniqueFruits.txt
